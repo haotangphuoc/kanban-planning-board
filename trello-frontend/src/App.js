9e5c7f132b2631ce116a5cd30cfc958e3d9db123
Ayah Abaza <ayah.abaza@dal.ca>
2023-06-04 14:29:00 -0300

    started landing page

--- trello-frontend/src/App.js
+++ trello-frontend/src/App.js
@@ -1,12 +1,55 @@
 import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Landing Page</h1>
-      </header>
+      <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Trello-Clone
+          </a>
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link" href="/">
+                Home
+              </a>
+
+              <a class="nav-link" href="/about">
+                About
+              </a>
+            </div>
+
+            <div class="navbar-nav ms-auto">
+              <a class="nav-link" href="/login">
+                Login
+              </a>
+
+              <a class="nav-link" href="/register">
+                Register
+              </a>
+
+              <a class="nav-link" href="/logout">
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
