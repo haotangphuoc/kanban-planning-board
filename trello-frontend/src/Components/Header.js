cf65fed72d7ec27b441fb4008d4cbc22533fc907
Ayah Abaza <ayah.abaza@dal.ca>
2023-06-08 15:20:16 -0300

    updated header

--- trello-frontend/src/Components/Header.js
+++ trello-frontend/src/Components/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="/">Trello-Clone</Navbar.Brand>
+        {/* DELETE THIS AFTER WE CONNECT TO DATABASE AND LOGIN FEATURE REDIRECTS USER */}
         <Navbar.Brand href="../Pages/Homepage.js">Homepage</Navbar.Brand>
-        <Navbar.Brand href="../Pages/Workspace.js">Workspace</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
       </Container>
