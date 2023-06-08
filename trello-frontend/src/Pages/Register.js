b90fa71053631d1ef65b05cbfe048d1e3bacb6dc
Muhammad Shafay Zulfiqar <mh278164@dal.ca>
2023-06-08 13:34:33 -0300

    added client-side authentication

--- trello-frontend/src/Pages/Register.js
+++ trello-frontend/src/Pages/Register.js
@@ -12,6 +12,8 @@ import InputGroup from "react-bootstrap/InputGroup";
 
 const Register = () => {
   const [validated, setValidated] = useState(false);
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -22,6 +24,31 @@ const Register = () => {
 
     setValidated(true);
   };
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+  
+    // Password requirements check
+    const hasUpperCase = /[A-Z]/.test(newPassword);
+    const hasLowerCase = /[a-z]/.test(newPassword);
+    const hasDigit = /\d/.test(newPassword);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(newPassword);
+    const isLongEnough = newPassword.length >= 8;
+  
+    // Set password state and display error message
+    setPassword(newPassword);
+    setErrorMessage(
+      !hasUpperCase ||
+      !hasLowerCase ||
+      !hasDigit ||
+      !hasSpecialChar ||
+      !isLongEnough
+        ? 'Password must contain at least one uppercase letter, one lowercase letter, one digit, one special character, and be at least 8 characters long.'
+        : ''
+    );
+  };
+  
+
   return (
     <div style={{ minHeight: "93vh" }}>
       <Container>
@@ -63,6 +90,9 @@ const Register = () => {
                     <Form.Control.Feedback type="invalid">
                       Please choose a username.
                     </Form.Control.Feedback>
+                    <Form.Control.Feedback type="valid">
+                      Looks good.
+                    </Form.Control.Feedback>
                   </InputGroup>
                 </Form.Group>
               </Row>
@@ -71,21 +101,24 @@ const Register = () => {
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" required />
                 <Form.Control.Feedback type="invalid">
-                  Please enter an email.
+                  Please enter a valid email. Make sure @ is included in your email.
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" required />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a password.
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group
-                className="mb-3"
-                controlId="formBasicCheckbox"
-              ></Form.Group>
+                <Form.Group className="mb-3" controlId="validationCustomPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={handlePasswordChange}
+                    isInvalid={errorMessage !== ""}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errorMessage}
+                  </Form.Control.Feedback>
+                </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>
