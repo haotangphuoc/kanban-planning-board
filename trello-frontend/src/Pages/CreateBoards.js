e6e9e062a8bb4d04ee7c44ba83d2662cdccb11cd
Muhammad Shafay Zulfiqar <mh278164@dal.ca>
2023-07-20 22:06:53 -0300

    added display boards feature

--- trello-frontend/src/Pages/CreateBoards.js
+++ trello-frontend/src/Pages/CreateBoards.js
@@ -15,49 +15,16 @@ const CreateBoards = () => {
   const [validated, setValidated] = useState(false);
   const [boardName, setBoardName] = useState("");
   const [boardDescription, setBoardDescription] = useState("");
-  const [userId, setUserId] = useState("");
   const [workspaceId, setWorkspaceId] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
-    const userData = localStorage.getItem("userData");
-    const workspaceName = localStorage.getItem("workspaceName");
-
-    if (userData && workspaceName) {
-      const { email } = JSON.parse(userData);
-      fetchUserIdAndWorkspaceId(email, workspaceName);
+    const selectedWorkspace = localStorage.getItem("selectedWorkspace");
+    if (selectedWorkspace) {
+      const workspace = JSON.parse(selectedWorkspace);
+      setWorkspaceId(workspace.id); // Extract workspace id from selected workspace
     }
   }, []);
-
-  const fetchUserIdAndWorkspaceId = async (email, workspaceName) => {
-    try {
-      const [userIdResponse, workspaceIdResponse] = await Promise.all([
-        fetch(`http://localhost:8001/api/findUserIdByEmail?email=${email}`),
-        fetch(
-          `http://localhost:8001/api/findWorkspaceIdByName?name=${workspaceName}`
-        ),
-      ]);
-
-      const userIdData = await userIdResponse.json();
-      const workspaceIdData = await workspaceIdResponse.json();
-
-      if (
-        userIdResponse.ok &&
-        workspaceIdResponse.ok &&
-        userIdData >= 0 &&
-        workspaceIdData >= 0
-      ) {
-        setUserId(userIdData);
-        setWorkspaceId(workspaceIdData);
-      } else {
-        console.error("Failed to fetch user ID or workspace ID");
-        // Handle error case
-      }
-    } catch (error) {
-      console.error("Failed to fetch user ID or workspace ID:", error);
-      // Handle error case
-    }
-  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -70,110 +37,92 @@ const CreateBoards = () => {
       return;
     }
 
-    // Connect to the database and save the board
-    const boardData = {
-      id: userId,
-      boards: [
-        {
-          title: boardName,
-          workspace: {
-            id: workspaceId,
-          },
-        },
-      ],
+    // Generate a unique ID for the new board
+    const boardId = new Date().getTime().toString();
+
+    // Create a new board object
+    const newBoard = {
+      id: boardId,
+      title: boardName,
+      description: boardDescription,
     };
 
-    try {
-      console.log(JSON.stringify(boardData));
-      const response = await fetch("http://localhost:8001/api/createBoard", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "POST,PATCH,OPTION",
-        },
-        body: JSON.stringify(boardData),
-      });
+    // Retrieve existing boards for this workspace from local storage
+    const existingBoards = JSON.parse(localStorage.getItem(`boards-${workspaceId}`)) || [];
 
-      if (response.ok) {
-        localStorage.setItem("boardName", JSON.stringify({ boardName }));
-        const data = await response;
-        console.log("Board created:", data);
-        navigate("/Pages/Workspace.js");
-        // Handle the response or update the state as needed
-      } else {
-        console.error("Failed to create board");
-        // Handle the error appropriately
-      }
-    } catch (error) {
-      console.error("Failed to create board:", error);
-      // Handle network errors or other exceptions
-    }
+    // Add the new board to the existing boards
+    const updatedBoards = [...existingBoards, newBoard];
+
+    // Save the updated boards back to local storage
+    localStorage.setItem(`boards-${workspaceId}`, JSON.stringify(updatedBoards));
+
+    // Navigate back to the workspace
+    navigate("/Pages/Workspace.js");
 
     setValidated(true);
   };
 
   return (
-    <div style={{ minHeight: "93vh" }}>
-      <Container>
-        <Row>
-          <div>
-            <Stack
-              className="mx-auto"
-              direction="horizontal"
-              style={{ paddingTop: 38 }}
-            >
-              <div style={{ paddingBottom: 12 }}>
-                <Card.Title>
-                  <Nav.Link href="../Pages/Workspace.js">
-                    &#60; Workspace
-                  </Nav.Link>
-                </Card.Title>
-              </div>
-            </Stack>
-            <h2 style={{ paddingTop: 38, paddingBottom: 38 }}>
-              Create a board
-            </h2>
-            <Card>
-              <Card.Body>
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                  <Row className="mb-3">
-                    <Form.Group as={Col} md="4" controlId="validationCustom01">
-                      <Form.Label>Board name</Form.Label>
-                      <Form.Control
-                        required
-                        type="text"
-                        placeholder="Enter a board name"
-                        value={boardName}
-                        onChange={(e) => setBoardName(e.target.value)}
-                      />
-                      <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                      <Form.Control.Feedback type="invalid">
-                        Please enter a board name.
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                  </Row>
-                  <Row className="mb-3">
-                    <Form.Group as={Col} controlId="validationCustom02">
-                      <Form.Label>Board description</Form.Label>
-                      <Form.Control
-                        as="textarea"
-                        rows={3}
-                        value={boardDescription}
-                        onChange={(e) => setBoardDescription(e.target.value)}
-                      />
-                    </Form.Group>
-                  </Row>
-                  <Button variant="primary" type="submit">
-                    Submit
-                  </Button>
-                </Form>
-              </Card.Body>
-            </Card>
-          </div>
-        </Row>
-      </Container>
-    </div>
+      <div style={{ minHeight: "93vh" }}>
+        <Container>
+          <Row>
+            <div>
+              <Stack
+                  className="mx-auto"
+                  direction="horizontal"
+                  style={{ paddingTop: 38 }}
+              >
+                <div style={{ paddingBottom: 12 }}>
+                  <Card.Title>
+                    <Nav.Link href="../Pages/Workspace.js">
+                      &#60; Workspace
+                    </Nav.Link>
+                  </Card.Title>
+                </div>
+              </Stack>
+              <h2 style={{ paddingTop: 38, paddingBottom: 38 }}>
+                Create a board
+              </h2>
+              <Card>
+                <Card.Body>
+                  <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Row className="mb-3">
+                      <Form.Group as={Col} md="4" controlId="validationCustom01">
+                        <Form.Label>Board name</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            placeholder="Enter a board name"
+                            value={boardName}
+                            onChange={(e) => setBoardName(e.target.value)}
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid">
+                          Please enter a board name.
+                        </Form.Control.Feedback>
+                      </Form.Group>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Group as={Col} controlId="validationCustom02">
+                        <Form.Label>Board description</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={3}
+                            value={boardDescription}
+                            onChange={(e) => setBoardDescription(e.target.value)}
+                        />
+                      </Form.Group>
+                    </Row>
+                    <Button variant="primary" type="submit">
+                      Submit
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </div>
+          </Row>
+        </Container>
+      </div>
   );
 };
 
--- trello-frontend/src/Pages/Workspace.js
+++ trello-frontend/src/Pages/Workspace.js
@@ -4,6 +4,7 @@ import { Container, Row, Nav, Stack, Card } from "react-bootstrap";
 const Workspace = () => {
   const [workspaceName, setWorkspaceName] = useState("");
   const [workspaceDescription, setWorkspaceDescription] = useState("");
+  const [boards, setBoards] = useState([]);
 
   useEffect(() => {
     const selectedWorkspace = localStorage.getItem("selectedWorkspace");
@@ -11,61 +12,66 @@ const Workspace = () => {
       const workspace = JSON.parse(selectedWorkspace);
       setWorkspaceName(workspace.name);
       setWorkspaceDescription(workspace.description);
+      // Retrieve boards from local storage for this workspace
+      const workspaceBoards = JSON.parse(localStorage.getItem(`boards-${workspace.id}`)) || [];
+      setBoards(workspaceBoards);
     }
   }, []);
 
   return (
-    <div style={{ minHeight: "93vh" }}>
-      <Container>
-        <Row>
-          <Stack
-            className="mx-auto"
-            direction="horizontal"
-            style={{ paddingTop: 38 }}
-          >
-            <div style={{ paddingBottom: 12 }}>
-              <Card.Title>
-                <Nav.Link href="../Pages/Homepage.js">&#60; Homepage</Nav.Link>
-              </Card.Title>
-            </div>
-          </Stack>
-          <div>
-            <h2 style={{ paddingTop: 38 }}>Workspaces</h2>
-            <br />
-            <h4>{workspaceName}</h4>
-            <p>{workspaceDescription}</p>
+      <div style={{ minHeight: "93vh" }}>
+        <Container>
+          <Row>
+            <Stack
+                className="mx-auto"
+                direction="horizontal"
+                style={{ paddingTop: 38 }}
+            >
+              <div style={{ paddingBottom: 12 }}>
+                <Card.Title>
+                  <Nav.Link href="../Pages/Homepage.js">&#60; Homepage</Nav.Link>
+                </Card.Title>
+              </div>
+            </Stack>
+            <div>
+              <h2 style={{ paddingTop: 38 }}>Workspaces</h2>
+              <br />
+              <h4>{workspaceName}</h4>
+              <p>{workspaceDescription}</p>
 
-            <br />
-            <Card>
-              <Card.Header>
-                <Nav variant="tabs" defaultActiveKey="#boards">
-                  <Nav.Item>
-                    <Nav.Link href="#boards">Boards</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link href="../Pages/CreateBoards.js">
-                      Create a board
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link href="../Pages/Members.js">Members</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link href="../Pages/Settings.js">Settings</Nav.Link>
-                  </Nav.Item>
-                </Nav>
-              </Card.Header>
-              <Card.Body>
-                <Card.Title>Board Title</Card.Title>
-                <Card.Text>Description</Card.Text>
-                <Card.Title>Board Title</Card.Title>
-                <Card.Text>Description</Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        </Row>
-      </Container>
-    </div>
+              <br />
+              <Card>
+                <Card.Header>
+                  <Nav variant="tabs" defaultActiveKey="#boards">
+                    <Nav.Item>
+                      <Nav.Link href="#boards">Boards</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="../Pages/CreateBoards.js">
+                        Create a board
+                      </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="../Pages/Members.js">Members</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link href="../Pages/Settings.js">Settings</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Card.Header>
+                <Card.Body>
+                  {boards.map((board) => (
+                      <div key={board.id}>
+                        <Card.Title>{board.title}</Card.Title>
+                        <Card.Text>{board.description}</Card.Text>
+                      </div>
+                  ))}
+                </Card.Body>
+              </Card>
+            </div>
+          </Row>
+        </Container>
+      </div>
   );
 };
 
